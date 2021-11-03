888807c3a6bb378a703cc7f750edaf8f9a9d98cf
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2021-11-03 16:38:14 +0200

    translations

--- Oven Interface/Views/Histeresis.Designer.cs
+++ Oven Interface/Views/Histeresis.Designer.cs
@@ -29,6 +29,7 @@ namespace Oven_Interface.Views
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Histeresis));
             this.allowChangeTempAfter = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.turnOffRelaysAfter = new System.Windows.Forms.NumericUpDown();
@@ -43,95 +44,66 @@ namespace Oven_Interface.Views
             // 
             // allowChangeTempAfter
             // 
-            this.allowChangeTempAfter.Location = new System.Drawing.Point(560, 62);
+            resources.ApplyResources(this.allowChangeTempAfter, "allowChangeTempAfter");
             this.allowChangeTempAfter.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
             this.allowChangeTempAfter.Name = "allowChangeTempAfter";
-            this.allowChangeTempAfter.Size = new System.Drawing.Size(88, 20);
-            this.allowChangeTempAfter.TabIndex = 44;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label2.Location = new System.Drawing.Point(7, 67);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(547, 13);
-            this.label2.TabIndex = 43;
-            this.label2.Text = "Дозволити знов змінити температуру через Х мілісекунд після попередньої зміни інт" +
-    "енсивності нагрівачів";
             // 
             // turnOffRelaysAfter
             // 
-            this.turnOffRelaysAfter.Location = new System.Drawing.Point(560, 31);
+            resources.ApplyResources(this.turnOffRelaysAfter, "turnOffRelaysAfter");
             this.turnOffRelaysAfter.Maximum = new decimal(new int[] {
             1000000,
             0,
             0,
             0});
             this.turnOffRelaysAfter.Name = "turnOffRelaysAfter";
-            this.turnOffRelaysAfter.Size = new System.Drawing.Size(88, 20);
-            this.turnOffRelaysAfter.TabIndex = 38;
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label7.Location = new System.Drawing.Point(7, 33);
+            resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(359, 13);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "Виключити терморегулюючі реле через Х мілісекунд після включення";
             // 
             // groupBox2
             // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.allowChangeTempAfter);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.turnOffRelaysAfter);
             this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(666, 100);
-            this.groupBox2.TabIndex = 59;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Налаштування Хістерезису";
             // 
             // saveCoefficientsButton
             // 
-            this.saveCoefficientsButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.saveCoefficientsButton.Location = new System.Drawing.Point(423, 118);
-            this.saveCoefficientsButton.Margin = new System.Windows.Forms.Padding(2);
+            resources.ApplyResources(this.saveCoefficientsButton, "saveCoefficientsButton");
             this.saveCoefficientsButton.Name = "saveCoefficientsButton";
-            this.saveCoefficientsButton.Size = new System.Drawing.Size(116, 41);
-            this.saveCoefficientsButton.TabIndex = 58;
-            this.saveCoefficientsButton.Text = "Зберегти";
             this.saveCoefficientsButton.UseVisualStyleBackColor = true;
             this.saveCoefficientsButton.Click += new System.EventHandler(this.saveCoefficientsButton_Click);
             // 
             // button55
             // 
-            this.button55.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button55.Location = new System.Drawing.Point(554, 118);
+            resources.ApplyResources(this.button55, "button55");
             this.button55.Name = "button55";
-            this.button55.Size = new System.Drawing.Size(124, 40);
-            this.button55.TabIndex = 60;
-            this.button55.Text = "Закрити";
             this.button55.UseVisualStyleBackColor = true;
             this.button55.Click += new System.EventHandler(this.button55_Click);
             // 
             // Histeresis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 167);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.saveCoefficientsButton);
             this.Controls.Add(this.button55);
             this.Name = "Histeresis";
-            this.Text = "Histeresis";
             ((System.ComponentModel.ISupportInitialize)(this.allowChangeTempAfter)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.turnOffRelaysAfter)).EndInit();
             this.groupBox2.ResumeLayout(false);
--- Oven Interface/Views/PinSettings.Designer.cs
+++ Oven Interface/Views/PinSettings.Designer.cs
@@ -32,35 +32,35 @@ namespace Oven_Interface.Views
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PinSettings));
             this.button55 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.saveCoefficientsButton = new System.Windows.Forms.Button();
+            this.waterCounterPin = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.waterValveOpenRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ventilationCloseRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ventilationOpenRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.thermalDecreaseRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.powerplantRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lightsRelayPin = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
             this.thermalIncreaseRelayPin = new System.Windows.Forms.NumericUpDown();
             this.thermalSensorChannel = new System.Windows.Forms.NumericUpDown();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.lightsRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.powerplantRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.thermalDecreaseRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.ventilationOpenRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label4 = new System.Windows.Forms.Label();
-            this.ventilationCloseRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
-            this.waterValveOpenRelayPin = new System.Windows.Forms.NumericUpDown();
-            this.label6 = new System.Windows.Forms.Label();
-            this.waterCounterPin = new System.Windows.Forms.NumericUpDown();
-            this.label9 = new System.Windows.Forms.Label();
+            this.saveCoefficientsButton = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waterCounterPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waterValveOpenRelayPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventilationCloseRelayPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventilationOpenRelayPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thermalDecreaseRelayPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.powerplantRelayPin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightsRelayPin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.thermalIncreaseRelayPin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.thermalSensorChannel)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lightsRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.powerplantRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.thermalDecreaseRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ventilationOpenRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ventilationCloseRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.waterValveOpenRelayPin)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.waterCounterPin)).BeginInit();
             this.SuspendLayout();
             // 
             // button55
@@ -72,6 +72,7 @@ namespace Oven_Interface.Views
             // 
             // groupBox2
             // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.waterCounterPin);
             this.groupBox2.Controls.Add(this.label9);
             this.groupBox2.Controls.Add(this.waterValveOpenRelayPin);
@@ -90,16 +91,113 @@ namespace Oven_Interface.Views
             this.groupBox2.Controls.Add(this.thermalSensorChannel);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.label7);
-            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.TabStop = false;
             // 
-            // saveCoefficientsButton
+            // waterCounterPin
             // 
-            resources.ApplyResources(this.saveCoefficientsButton, "saveCoefficientsButton");
-            this.saveCoefficientsButton.Name = "saveCoefficientsButton";
-            this.saveCoefficientsButton.UseVisualStyleBackColor = true;
-            this.saveCoefficientsButton.Click += new System.EventHandler(this.saveCoefficientsButton_Click);
+            resources.ApplyResources(this.waterCounterPin, "waterCounterPin");
+            this.waterCounterPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.waterCounterPin.Name = "waterCounterPin";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // waterValveOpenRelayPin
+            // 
+            resources.ApplyResources(this.waterValveOpenRelayPin, "waterValveOpenRelayPin");
+            this.waterValveOpenRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.waterValveOpenRelayPin.Name = "waterValveOpenRelayPin";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // ventilationCloseRelayPin
+            // 
+            resources.ApplyResources(this.ventilationCloseRelayPin, "ventilationCloseRelayPin");
+            this.ventilationCloseRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.ventilationCloseRelayPin.Name = "ventilationCloseRelayPin";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // ventilationOpenRelayPin
+            // 
+            resources.ApplyResources(this.ventilationOpenRelayPin, "ventilationOpenRelayPin");
+            this.ventilationOpenRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.ventilationOpenRelayPin.Name = "ventilationOpenRelayPin";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // thermalDecreaseRelayPin
+            // 
+            resources.ApplyResources(this.thermalDecreaseRelayPin, "thermalDecreaseRelayPin");
+            this.thermalDecreaseRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.thermalDecreaseRelayPin.Name = "thermalDecreaseRelayPin";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // powerplantRelayPin
+            // 
+            resources.ApplyResources(this.powerplantRelayPin, "powerplantRelayPin");
+            this.powerplantRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.powerplantRelayPin.Name = "powerplantRelayPin";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // lightsRelayPin
+            // 
+            resources.ApplyResources(this.lightsRelayPin, "lightsRelayPin");
+            this.lightsRelayPin.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.lightsRelayPin.Name = "lightsRelayPin";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
             // thermalIncreaseRelayPin
             // 
@@ -131,110 +229,12 @@ namespace Oven_Interface.Views
             resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
             // 
-            // lightsRelayPin
+            // saveCoefficientsButton
             // 
-            resources.ApplyResources(this.lightsRelayPin, "lightsRelayPin");
-            this.lightsRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.lightsRelayPin.Name = "lightsRelayPin";
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            // 
-            // powerplantRelayPin
-            // 
-            resources.ApplyResources(this.powerplantRelayPin, "powerplantRelayPin");
-            this.powerplantRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.powerplantRelayPin.Name = "powerplantRelayPin";
-            // 
-            // label2
-            // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
-            // 
-            // thermalDecreaseRelayPin
-            // 
-            resources.ApplyResources(this.thermalDecreaseRelayPin, "thermalDecreaseRelayPin");
-            this.thermalDecreaseRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.thermalDecreaseRelayPin.Name = "thermalDecreaseRelayPin";
-            // 
-            // label3
-            // 
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.Name = "label3";
-            // 
-            // ventilationOpenRelayPin
-            // 
-            resources.ApplyResources(this.ventilationOpenRelayPin, "ventilationOpenRelayPin");
-            this.ventilationOpenRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.ventilationOpenRelayPin.Name = "ventilationOpenRelayPin";
-            // 
-            // label4
-            // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Name = "label4";
-            // 
-            // ventilationCloseRelayPin
-            // 
-            resources.ApplyResources(this.ventilationCloseRelayPin, "ventilationCloseRelayPin");
-            this.ventilationCloseRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.ventilationCloseRelayPin.Name = "ventilationCloseRelayPin";
-            // 
-            // label5
-            // 
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.Name = "label5";
-            // 
-            // waterValveOpenRelayPin
-            // 
-            resources.ApplyResources(this.waterValveOpenRelayPin, "waterValveOpenRelayPin");
-            this.waterValveOpenRelayPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.waterValveOpenRelayPin.Name = "waterValveOpenRelayPin";
-            // 
-            // label6
-            // 
-            resources.ApplyResources(this.label6, "label6");
-            this.label6.Name = "label6";
-            // 
-            // waterCounterPin
-            // 
-            resources.ApplyResources(this.waterCounterPin, "waterCounterPin");
-            this.waterCounterPin.Maximum = new decimal(new int[] {
-            50,
-            0,
-            0,
-            0});
-            this.waterCounterPin.Name = "waterCounterPin";
-            // 
-            // label9
-            // 
-            resources.ApplyResources(this.label9, "label9");
-            this.label9.Name = "label9";
+            resources.ApplyResources(this.saveCoefficientsButton, "saveCoefficientsButton");
+            this.saveCoefficientsButton.Name = "saveCoefficientsButton";
+            this.saveCoefficientsButton.UseVisualStyleBackColor = true;
+            this.saveCoefficientsButton.Click += new System.EventHandler(this.saveCoefficientsButton_Click);
             // 
             // PinSettings
             // 
@@ -246,15 +246,15 @@ namespace Oven_Interface.Views
             this.Name = "PinSettings";
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waterCounterPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waterValveOpenRelayPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventilationCloseRelayPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ventilationOpenRelayPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thermalDecreaseRelayPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.powerplantRelayPin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightsRelayPin)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.thermalIncreaseRelayPin)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.thermalSensorChannel)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lightsRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.powerplantRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.thermalDecreaseRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ventilationOpenRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ventilationCloseRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.waterValveOpenRelayPin)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.waterCounterPin)).EndInit();
             this.ResumeLayout(false);
 
         }
